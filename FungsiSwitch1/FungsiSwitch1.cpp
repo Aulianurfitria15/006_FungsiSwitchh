5989f25960bae94c7e2ed91479839bcf3e360d03
Aulianurfitria15 <aulianurfitria17@gmail.com>
2024-03-26 17:47:26 +0700

    memasukkan penjumlahn

--- FungsiSwitch1/FungsiSwitch1.cpp
+++ FungsiSwitch1/FungsiSwitch1.cpp
@@ -13,3 +13,20 @@ void input()
     cin >> bilB;
 }
 
+int penjumlahan(int a, int b)
+{
+    return a + b;
+}
+int pengurangan(int a, int b)
+{
+    return a - b;
+}
+int perkalian(int a, int b)
+{
+    return a * b;
+}
+int pembagian(int a, int b)
+{
+    return a / b;
+}
+
